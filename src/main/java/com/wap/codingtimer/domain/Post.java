5f882947deefce20940a167f1508d9f9e5a83af9
spiky-haired-Pyramid-head <qns0147@gmail.com>
2022-11-01 23:00:05 +0900

    feat: Comment, Post 엔티티 생성

---
+++ src/main/java/com/wap/codingtimer/domain/Post.java
@@ -0,0 +1,36 @@
+package com.wap.codingtimer.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+public class Post {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "post_id")
+    private Long id;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+    private List<Comment> comments = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+
+    private String topic;
+
+    private String content;
+
+    private String category;
+
+    private int likes;
+
+    private LocalDateTime date;
+}
