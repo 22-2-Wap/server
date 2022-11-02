72ec5b844300049472a43f6769e68fac67a2f339
nuyh <hjaehyun25@gmail.com>
2022-11-02 18:48:00 +0900

    feat: Friend, Member, Timer Repository 추가

---
+++ src/main/java/com/wap/codingtimer/repository/TimerRepository.java
@@ -0,0 +1,10 @@
+package com.wap.codingtimer.repository;
+
+import com.wap.codingtimer.domain.Timer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TimerRepository extends JpaRepository<Timer, Long> {
+    Timer findTopByMember_IdOrderByFirstStartedTime(Long member_id);
+}
